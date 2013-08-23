c65df1e8ea3dac3b9bc2507e8e54e0cbed2a1f31
Richard Price <richardprice@gmail.com>
2013-08-23 14:07:40 +0100

    Initial Commit

---
+++ Bootstrapper.cs
@@ -0,0 +1,22 @@
+﻿using Nancy.Conventions;
+
+namespace Insanity
+{
+    using Nancy;
+
+    public class Bootstrapper : DefaultNancyBootstrapper
+    {
+        // The bootstrapper enables you to reconfigure the composition of the framework,
+        // by overriding the various methods and properties.
+        // For more information https://github.com/NancyFx/Nancy/wiki/Bootstrapper
+
+        protected override void ConfigureConventions(NancyConventions nancyConventions)
+        {
+            base.ConfigureConventions(nancyConventions);
+
+            nancyConventions.StaticContentsConventions.Add(
+                StaticContentConventionBuilder.AddDirectory("/Assets")
+            );
+        }
+    }
+}
---
+++ IndexModule.cs
@@ -0,0 +1,15 @@
+﻿namespace Insanity
+{
+    using Nancy;
+
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = parameters =>
+            {
+                return View["~/views/alt-index.cshtml"];
+            };  
+        }
+    }
+}
